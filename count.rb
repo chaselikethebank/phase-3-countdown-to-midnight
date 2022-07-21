073fba3f0ec45820bec1db3c0c5315ea48bef7d5
Chase <chasedemaster.tech@gmail.com>
2022-07-21 17:54:00 -0500

    commit tree 4edfa28d0fd5627a3cf58dd474e2bf4184991d7f

---
+++ count.rb
@@ -0,0 +1,5 @@
+x = 1
+while x < 100
+  puts "#{x} is way less that what you think it is"
+  x += 1
+end
--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
 def countdown
+    i = 10
+    if i > 1
+        puts "#{number} SECONDS till MIDNIGHT!"
+        i -= 1
+    elsif i >= 1
+        puts "#{number} LAST SECOND till MIDNIGHT!"
+        i -= 1
+    else i = 0
+        puts "HAPPY NEW YEAR!"
+    end
 end
+
+
+# def countdown_with_sleep
+# end
